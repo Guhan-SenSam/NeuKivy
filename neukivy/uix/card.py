2f116e332c0b49ad7d1ed3210fff0085375fd899
sagar H M <55372566+sagarhm@users.noreply.github.com>
2021-06-25 16:35:28 +0530

    Update card.py

--- neukivy/uix/card.py
+++ neukivy/uix/card.py
@@ -1,12 +1,42 @@
 from kivy.lang import Builder
 from kivy.properties import ListProperty, NumericProperty
 from kivy.uix.boxlayout import BoxLayout
-from neukivy.uix.behaviors.neumorph import NeuMorphRoundedRectangle
+from kivy.uix.anchorlayout import AnchorLayout
+from neukivy.uix.behaviors.neumorph import (
+    NeuMorphCircular,
+    NeuMorphRectangle,
+    NeuMorphRoundedRectangle,
+)
 from neukivy.uix.behaviors.themeablebehavior import ThemeableBehavior
 
 Builder.load_string(
 """
 <NeuCard>:
+    canvas.before:
+        Clear
+        Color:
+        Rectangle:
+            size:self.light_shadow_size
+            pos:self.light_shadow_pos
+            texture:self.light_shadow
+        Color:
+            rgba:1,1,1,1
+        Rectangle:
+            size:self.dark_shadow_size
+            pos:self.dark_shadow_pos
+            texture:self.dark_shadow
+    canvas:
+        Color:
+            rgba:(1,1,1,1) if self.elevation and self.elevation < 0 else self.comp_color
+        Rectangle:
+            size:self.size
+            pos:self.pos
+            #radius:self.radius,self.radius,self.radius,self.radius
+            texture:self.border_texture if self.elevation and self.elevation < 0 else None
+        Color:
+    size_hint:None,None
+    size:200,200
+<NeuCardRounded>:
     canvas.before:
         Clear
         Color:
@@ -30,15 +60,36 @@ Builder.load_string(
             texture:self.border_texture if self.elevation and self.elevation < 0 else None
         Color:
     size_hint:None,None
-    size:200,200
-
-
-
+    size:200,200 
+<NeuCardCircular>:
+    canvas.before:
+        Clear
+        Color:
+        Rectangle:
+            size:self.light_shadow_size
+            pos:self.light_shadow_pos
+            texture:self.light_shadow
+        Color:
+            rgba:1,1,1,1
+        Rectangle:
+            size:self.dark_shadow_size
+            pos:self.dark_shadow_pos
+            texture:self.dark_shadow
+    canvas:
+        Color:
+            rgba:(1,1,1,1) if self.elevation and self.elevation < 0 else self.comp_color
+        Ellipse:
+            size:self.size
+            pos:self.pos
+            texture:self.border_texture if self.elevation and self.elevation < 0 else None
+        Color:
+    size_hint:None,None
+    size:200,200         
 """
 )
 
 
-class NeuCard(BoxLayout,ThemeableBehavior,NeuMorphRoundedRectangle):
+class NeuCard(BoxLayout,ThemeableBehavior,NeuMorphRectangle):
 
     comp_color = ListProperty([0, 0, 0, 0])
 
@@ -49,7 +100,42 @@ class NeuCard(BoxLayout,ThemeableBehavior,NeuMorphRoundedRectangle):
     radius = NumericProperty("20dp")
     """
     Radius of the edges of the card
+    attr:`radius` is an :class:`~kivy.properties.NumericProperty`
+    and defaults to `'20dp'`.
+    """
 
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.elevation = 2
+class NeuCardRounded(BoxLayout,ThemeableBehavior,NeuMorphRoundedRectangle):
+
+    comp_color = ListProperty([0, 0, 0, 0])
+
+    dark_color = ListProperty([0, 0, 0, 0])
+
+    light_color = ListProperty([0, 0, 0, 0])
+
+    radius = NumericProperty("20dp")
+    """
+    Radius of the edges of the card
+    attr:`radius` is an :class:`~kivy.properties.NumericProperty`
+    and defaults to `'20dp'`.
+    """
+
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.elevation = 2
+class NeuCardCircular(BoxLayout,ThemeableBehavior,NeuMorphCircular):
+
+    comp_color = ListProperty([0, 0, 0, 0])
+
+    dark_color = ListProperty([0, 0, 0, 0])
+
+    light_color = ListProperty([0, 0, 0, 0])
+
+    radius = NumericProperty("20dp")
+    """
+    Radius of the edges of the card
     attr:`radius` is an :class:`~kivy.properties.NumericProperty`
     and defaults to `'20dp'`.
     """
